29d17cb459750e2991b73cf6bffdd52d8fa43e3b
pittman31 <77406817+pittman31@users.noreply.github.com>
2024-12-01 16:17:44 -0500

    Added Flask Endpoints For Google Docs Integration
    Added an axios call in Login.js for google authenticaiton.
    
    Added a return call in Document.py exportDocumentAsTxt().
    
    Added google_login(), get_document() functionality in app.py.
    
    Added a call to Document.insertTextAtEnd to update the document with the textract response in app.py upload_image().

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,18 @@
-
 import json
 from flask_cors import CORS
 from flask import Flask,request
 from endpoints import text_extraction as text_extraction
 from endpoints import upload_to_s3 as s3_upload
-
+from GoogleAPI.src import GoogleHelperFunctions
+from GoogleAPI.src import Document
 
 app = Flask(__name__)#Flask application and  enabling cors
 CORS(app,origins=["http://localhost:3000"])
+
+myDocument = None   #Document Object
+creds = None    #Credentials for the Google Servers
+docsService = None  #Docs Service object
+driveService = None #Drive Service Object
 
 @app.route('/upload_image', methods=['Post'])#upload image notes route
 def upload_image():
@@ -21,8 +26,10 @@ def upload_image():
 
         textract_response=text_extraction.text_extraction(notesfile.filename)#calling textextract if response is succesful
 
-        print( "textract_response",textract_response)#printing textract response
+        print("textract_response",textract_response)#printing textract response
         
+        globals()['myDocument'].insertTextAtEnd(textract_response)#Insert the textract response at the end of the document
+
         return {
          'status': 'success',
          'Message': 'text extraction successful'
@@ -35,11 +42,20 @@ def upload_image():
     
 @app.route('/get_document', methods=['Post'])
 def get_document():
-    request_data= request.get_json()  
-    id = request_data.get('id')
-    print("id",id)
-    return {'status': 'success',
-            'content':" updated data example content"}
+
+    #get the latest document updates
+    myDocument.getDocumentUpdates()
+
+    #Export Document as text
+    documentText = myDocument.exportDocumentAsTxt()
+
+    #request_data= request.get_json()  
+    #id = request_data.get('id')
+    #print("id",id)
+    return {
+        'status': 'success',
+        'content': documentText
+        }
 
 
 
@@ -47,6 +63,27 @@ def get_document():
 def update_document():
     pass
 
-       
+
+@app.route('/google_login', methods=['Get'])#login route
+def google_login():
+
+    #Setup Authentication Credentials for Google API access
+    creds = GoogleHelperFunctions.setupCredentials()
+
+    #Build Google Drive and Docs Services
+    driveService, docsService = GoogleHelperFunctions.buildServices(creds)
+
+    #Instantiate the document object
+    globals()['myDocument'] = Document.Document(GoogleHelperFunctions.DOCUMENT_ID, driveService, docsService, creds)
+
+    if(1):
+        return {
+         'status': 'success',
+        }
+    else:
+        return {
+        'status': 'failed'
+        }
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
