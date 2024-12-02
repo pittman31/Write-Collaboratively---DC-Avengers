0d42a492e57a3e6a3f73502a643be02dbb2cfaac
pittman31 <77406817+pittman31@users.noreply.github.com>
2024-12-02 10:58:01 -0500

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -6,8 +6,7 @@ from GoogleAPI.src import GoogleHelperFunctions
 from GoogleAPI.src import Document
 
 app = Flask(__name__)#Flask application and  enabling cors
-#CORS(app, origins=["*"])
-CORS(app, resources={r"/*": {"origins": "http://54.221.20.67:3000"}})
+CORS(app, origins=["*"])
 
 myDocument = None   #Document Object
 creds = None    #Credentials for the Google Servers
