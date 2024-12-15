8f177145655823c78af8f8412f75930ef4da4bc5
pittman31 <77406817+pittman31@users.noreply.github.com>
2024-12-15 18:32:04 -0500

    Changing IP address

--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -59,7 +59,7 @@ const ImageUpload = () => {
 
     console.log('Submitted Data:', data.getAll('docTitle'), data.getAll('imagefile'));
 
-    axios.post("http://34.233.133.74:5000/upload_image", data)
+    axios.post("http://98.84.107.116:5000/upload_image", data)
 
       .then(response => {
 
